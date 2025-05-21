48d7e1fa1c41de325d1784abf15dfe8bc689e6c3
Dionexu <buyfuture.office@gmail.com>
2025-05-21 19:06:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -135,18 +135,18 @@ async def handle_coin_input(message: types.Message):
 
     coin_input = message.text.lower().strip()
 
+    if coin_input == "готово":
+        user_data["mode"] = None
+        await message.answer("✅ Монети збережено. Тепер натисніть 'Дивитися ціни'.")
+        return
+
     global coin_list_cache, symbol_to_id_map
     if not coin_list_cache:
         async with aiohttp.ClientSession() as session:
             url = "https://api.coingecko.com/api/v3/coins/list"
             async with session.get(url) as resp:
                 coin_list_cache = await resp.json()
-                symbol_to_id_map = {c['symbol'].lower(): c['id'] for c in coin_list_cache}
-
-    if coin_input == "готово":
-        user_data["mode"] = None
-        await message.answer("✅ Монети збережено. Тепер натисніть 'Дивитися ціни'.")
-        return
+        symbol_to_id_map = {c['symbol'].lower(): c['id'] for c in coin_list_cache}
 
     coin_id = symbol_to_id_map.get(coin_input)
 
